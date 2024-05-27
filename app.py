911ad0b1294e382925fc091ea5a7f0809b7ad807
Baruk Freitas <baruk.freitas@gmail.com>
2024-05-27 12:57:09 -0300

    Rotas consertadas

--- app.py
+++ app.py
@@ -3,7 +3,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html")
 
-app.run()
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/reservaConcluida")
+def reservaConcluida():
+    return render_template("reservaConcluida.html")
+
+@app.route("/reservar")
+def reservar():
+    return render_template("reservar.html")
+
+
+
+app.run(debug=True)
